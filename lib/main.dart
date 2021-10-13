0d287d5138cd0430acdb8a2c13d5c6ad6ade547e
FlutLab.io <>
2021-10-13 06:36:02 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,10 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Text('Show FlushBar'),
           onPressed: () {
             Flushbar(
-              icon: Icon(Icons.access_alarm, color: Colors.white),
+              icon: Icon(
+                Icons.access_alarm,
+                color: Colors.white,
+              ),
               title: 'Flushbar',
               //message: 'This is flushbar message',
               messageText: Text('This is flushbar message',
