355bc68c6526ac2624d4818a30a8cc8c66dc50aa
FlutLab.io <>
2021-10-13 06:28:27 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: RaisedButton(
           child: Text('Show FlushBar'),
           onPressed: () {
-            Flashbar();
+            Flushbar();
           },
         ),
       ),
